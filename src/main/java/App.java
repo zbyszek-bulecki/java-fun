fa8e474405c6be088290578422d6cdf57588e24e
zbyszek-bulecki <zbyszek.bulecki@gmail.com>
2022-11-06 16:27:24 +0100

    logging errors added

---
+++ src/main/java/App.java
@@ -0,0 +1,53 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class App {
+
+    private static final String HOST = "127.0.0.1";
+    private static final int PORT = 8080;
+    private static final int BACKLOG = 0;
+
+    public static void main(String[] args) throws IOException {
+
+        final Logger logger = Logger.getLogger(App.class.getName()); //java.util.logging.Logger
+        List<Socket> socketList = Collections.synchronizedList(new ArrayList<>()); //rozwiązanie 1, alternatywą może być concurrent list
+
+        // jeżeli używamy kolekcji na wielu wątkach to trzeba zadbać o bezpieczeństwo dostępu współbierznego
+        ServerSocket serverSocket = new ServerSocket(PORT, BACKLOG, InetAddress.getByName(HOST));
+        logger.log(Level.INFO, "Listening on port {0} : {1} with a connection backlog of {2}", new Object[]{HOST, PORT, BACKLOG});
+
+        new Thread(() -> {
+            while (true) {
+                try {
+                    socketList.add(serverSocket.accept());// daj mi deskryptor połączenia przychodzącego
+                    logger.log(Level.INFO, "Socket added to list.");
+                } catch (IOException e) {
+                    logger.log(Level.SEVERE, "Unable to obtain socket descriptor. {0}", e);
+                    throw new RuntimeException(e);
+                }
+            }
+        }).start();
+
+        while (true) {
+            socketList.forEach((socket -> {
+                InputStream socketInputStream = null;
+                try {
+                    socketInputStream = socket.getInputStream();
+                    if (socketInputStream.available() > 0) {
+                        System.out.println((char) socketInputStream.read());
+                    }
+                } catch (IOException e) {
+                    logger.log(Level.SEVERE, "No input stream available. {0}", e);
+                    throw new RuntimeException(e);
+                }
+            }));
+        }
+    }
+}
