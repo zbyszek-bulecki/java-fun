cc38e265448f7bc7acd93c5ebd0790a7972904ba
zbyszek-bulecki <zbyszek.bulecki@gmail.com>
2023-02-10 22:20:09 +0100

    adding a local class

--- TestClass.java
+++ TestClass.java
@@ -4,3 +4,15 @@ public class TestClass {
 	System.out.println("Hello from EMACS!");
     }
 }
+
+class Animal {
+    String name;
+
+    public String getName(){
+	return name;
+    }
+
+    public void setName(String newName){
+	name = newName;
+    }
+}
